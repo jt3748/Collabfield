90701441f6dfa76d7274cd8c04b212b4fa1baaaa
Justin Taj <justintaj@yahoo.com>
2019-03-05 14:14:16 -0800

    Create Private::Conversation and Private::Message models - Define associations between User, Private::Conversation   and Private::Message models - Define private_conversations and private_messages tables

---
+++ app/models/private/message.rb
@@ -0,0 +1,8 @@
+class Private::Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :conversation,
+             class_name: 'Private::Conversation',
+             foreign_key: :conversation_id
+
+  self.table_name = 'private_messages'
+end
