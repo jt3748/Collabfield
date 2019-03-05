b291c12418c32e99d31648106e9e9fa1288921e5
Justin Taj <justintaj@yahoo.com>
2019-03-04 19:45:27 -0800

    add login logout signup features and users factories and navigation_helper_spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,12 +79,15 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   require 'capybara/poltergeist'
-  require 'factory_girl_rails'
+  require 'factory_bot_rails'
   require 'capybara/rspec'
 
   config.include Devise::Test::IntegrationHelpers, type: :feature
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   Capybara.javascript_driver = :poltergeist
   Capybara.server = :puma
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, {js_errors: false})
+  end
 
 end
